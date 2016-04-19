889eb84ee948bfe930e0c3e576377515535b20fb
Derek Graham <derekgraham@mac.com>
2016-04-19 10:26:02 -0700

    removed commented, dead, test code

--- js/sales.js
+++ js/sales.js
@@ -21,11 +21,8 @@ var pikePlaceStore =  {
       this.totalCookiesPerDay += this.estimatedCookiesPerHour[i];
     }
     console.log(this.totalCookiesPerDay);
-
   },
-
 };
-
 
 var seaTacAirportStore =  {
   storeName : 'SeaTac Airport',
@@ -45,13 +42,10 @@ var seaTacAirportStore =  {
     for ( var i = 0; i < this.storeOpenHours; i++) // fixme assumes store open before noon, close after noon.
     {
       this.estimatedCookiesPerHour[i] = Math.round(this.randomCustomersPerHour() * this.averageCookiesPerCustomer);
-    //  console.log( i + ', ' + this.estimatedCookiesPerHour[i]);
       this.totalCookiesPerDay += this.estimatedCookiesPerHour[i];
     }
     console.log(this.totalCookiesPerDay);
-
   },
-
 };
 
 var southCenterStore =  {
@@ -72,13 +66,9 @@ var southCenterStore =  {
     for ( var i = 0; i < this.storeOpenHours; i++) // fixme assumes store open before noon, close after noon.
     {
       this.estimatedCookiesPerHour[i] = Math.round(this.randomCustomersPerHour() * this.averageCookiesPerCustomer);
-    //  console.log( i + ', ' + this.estimatedCookiesPerHour[i]);
       this.totalCookiesPerDay += this.estimatedCookiesPerHour[i];
     }
-    console.log(this.totalCookiesPerDay);
-
   },
-
 };
 
 var bellevueSquareStore =  {
@@ -102,8 +92,6 @@ var bellevueSquareStore =  {
     //  console.log( i + ', ' + this.estimatedCookiesPerHour[i]);
       this.totalCookiesPerDay += this.estimatedCookiesPerHour[i];
     }
-    console.log(this.totalCookiesPerDay);
-
   },
 };
 
@@ -125,64 +113,12 @@ var alkiStore =  {
     for ( var i = 0; i < this.storeOpenHours; i++) // fixme assumes store open before noon, close after noon.
     {
       this.estimatedCookiesPerHour[i] = Math.round(this.randomCustomersPerHour() * this.averageCookiesPerCustomer);
-    //  console.log( i + ', ' + this.estimatedCookiesPerHour[i]);
       this.totalCookiesPerDay += this.estimatedCookiesPerHour[i];
     }
-    console.log(this.totalCookiesPerDay);
-
   },
-  testHours : function() {
-    this.simulateCookies();
-    for (i = 0 ; i < this.storeOpenHours ; i ++ ){
-      if( i + this.storeOpenTime <= 12 ){
-        console.log(i + this.storeOpenTime + 'AM');
-      } else {
-        console.log(i + this.storeOpenTime - 12 + 'PM');
-      }
-    }
-  },
-  testRandom : function() {
-    var test = 0;
-    do {
-      test = this.randomCustomersPerHour();
-      console.log( test );
-    } while (test != this.minCustomersPerHour && test != this.maxCustomersPerHour);
-  },
-  // salesReport : function() {
-  //   this.simulateCookies();
-  //   var h2El = document.createElement('h2');
-  //   h2El.textContent = 'Sales Report';
-  //   document.body.appendChild(h2El);
-  //
-  //   var salesListTopEl = document.createElement('ul');
-  //   salesListTopEl.textContent = 'Sales Report - Cookies sold by hour per store ';
-  //   document.body.appendChild(salesListTopEl);
-  //
-  //   var salesListTopEl = document.createElement('ul');
-  //   salesListTopEl.textContent = '';
-  //   document.body.appendChild(salesListTopEl);
-  //
-  //   var salesListInsideEl = document.createElement('ul');
-  //   salesListInsideEl.textContent = this.storeName;
-  //   salesListTopEl.appendChild(salesListInsideEl);
-  //
-  //   var salesListItemEL = {};
-  //   //console.log(this.estimatedCookiesPerHour.length);
-  //   for ( var i = 0 ; i < this.estimatedCookiesPerHour.length ; i ++){
-  //     salesListItemEL = document.createElement('li');
-  //     salesListItemEL.textContent = ( function(storeOpenTime) { if(storeOpenTime < 12) { return storeOpenTime + 'am: '; } else if (storeOpenTime > 12 ) { return (storeOpenTime - 12 + 'pm: ');} else if (storeOpenTime = 12 ) { return storeOpenTime + 'pm: '; };}(i + this.storeOpenTime) ) + this.estimatedCookiesPerHour[i] + ' cookies';
-  //     salesListInsideEl.appendChild(salesListItemEL);
-  //     //console.log(this.estimatedCookiesPerHour[i]);
-  //   }
-  //
-  //   var salesTotalEl = document.createElement('li');
-  //   salesTotalEl.textContent = 'Total: ' + this.totalCookiesPerDay;
-  //   salesListInsideEl.appendChild(salesTotalEl);
-  // }
 };
 
 var stores = [pikePlaceStore,seaTacAirportStore,southCenterStore,bellevueSquareStore,alkiStore];
-//alkiStore.salesReport2();
 
 function salesReport( myStores ) {
   var salesListTopEl = document.createElement('ul');
@@ -192,25 +128,24 @@ function salesReport( myStores ) {
   for (var i = 0; i < stores.length; i ++){
     stores[i].simulateCookies();
     var salesListInsideEl = document.createElement('ul');
-    salesListInsideEl.textContent = myStores[i].storeName + ' Store';
     salesListTopEl.appendChild(salesListInsideEl);
 
-    var makeBrEl = document.createElement('br');
-    salesListInsideEl.appendChild(makeBrEl);
+    var liBuild = document.createElement('p');
+    liBuild.textContent = myStores[i].storeName + ' Store';
+    salesListInsideEl.appendChild(liBuild);
 
-    makeBrEl = document.createElement('br');
-    salesListInsideEl.appendChild(makeBrEl);
+    var makeBrEl = document.createElement('br');
 
     for ( var j = 0 ; j < stores[i].estimatedCookiesPerHour.length ; j ++){
-      var liBuild = ( function(storeOpenTime) { if(storeOpenTime < 12) { return storeOpenTime + 'am: '; } else if (storeOpenTime > 12 ) { return (storeOpenTime - 12 + 'pm: ');} else if (storeOpenTime = 12 ) { return storeOpenTime + 'pm: '; };}(j + stores[i].storeOpenTime) ) + stores[i].estimatedCookiesPerHour[j] + ' cookies';
+      liBuild = ( function(storeOpenTime) { if(storeOpenTime < 12) { return storeOpenTime + 'am: '; } else if (storeOpenTime > 12 ) { return (storeOpenTime - 12 + 'pm: ');} else if (storeOpenTime = 12 ) { return storeOpenTime + 'pm: '; };}(j + stores[i].storeOpenTime) ) + stores[i].estimatedCookiesPerHour[j] + ' cookies';
       salesListInsideEl.innerHTML = salesListInsideEl.innerHTML + '<li>' + liBuild + '</li>';
-      console.log(stores[i].estimatedCookiesPerHour[j]);
+      // console.log(stores[i].estimatedCookiesPerHour[j]);
     }
     var salesTotalEl = document.createElement('li');
     salesTotalEl.textContent = 'Total: ' + stores[i].totalCookiesPerDay + ' cookies';
     salesListInsideEl.appendChild(salesTotalEl);
 
-    makeBrEl = document.createElement('br');
+  //  makeBrEl = document.createElement('br');
     salesListInsideEl.appendChild(makeBrEl);
   }
 };
